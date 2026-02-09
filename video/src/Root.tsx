d0f158b038ebc78ae524f2b087cf5f826a8961b0
0xJMC <julio.cruz@eb-ms.net>
2026-02-08 23:28:11 -0500

    feat: improve video with better effects and PerkyFi benefits explanation

--- video/src/Root.tsx
+++ video/src/Root.tsx
@@ -7,7 +7,7 @@ export const RemotionRoot: React.FC = () => {
       <Composition
         id="PerkyFiDemo"
         component={PerkyFiVideo}
-        durationInFrames={30 * 120} // 2 minutes at 30fps
+        durationInFrames={2100} // 70 seconds at 30fps
         fps={30}
         width={1920}
         height={1080}
